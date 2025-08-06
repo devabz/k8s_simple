222bac7180e807ff6240e6f9584c809e695d9dca
Abraham Korh <abraham.korh@sikt.no>
2025-08-06 22:11:11 +0200

    updated scripts

--- resources/vms_create.sh
+++
@@ -1,7 +0,0 @@
-source .env
-
-curl -X POST \
-    "https://api.digitalocean.com/v2/droplets" \
-    -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
-    --json @resources/vms.json \
-    > vms_create.json
--- resources/vms_list.sh
+++
@@ -1,7 +0,0 @@
-source .env
-
-curl -X GET \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
-  "https://api.digitalocean.com/v2/droplets?page=1&per_page=10" \
-  > vms_list.json
